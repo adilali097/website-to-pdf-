f2e42b7061d8699f0a77926ec27b40486cbd0575
Muhammed ALi <155841427+adilali097@users.noreply.github.com>
2024-02-12 17:53:43 +0530

    Update pytre.py

--- pytre.py
+++ pytre.py
@@ -1,4 +1,4 @@
-import pdfkit
+from weasyprint import HTML
 
 # URL of the website to convert
 url = 'https://www.geeksforgeeks.org/pattern-programs-in-c/'
@@ -6,18 +6,9 @@ url = 'https://www.geeksforgeeks.org/pattern-programs-in-c/'
 # Path to save the PDF file
 pdf_path = 'geeksforgeeks_patterns_in_c.pdf'
 
-# Options for wkhtmltopdf
-options = {
-    'page-size': 'A4',
-    'margin-top': '0.75in',
-    'margin-right': '0.75in',
-    'margin-bottom': '0.75in',
-    'margin-left': '0.75in',
-}
-
 try:
-    # Convert the website to PDF
-    pdfkit.from_url(url, pdf_path, options=options)
+    # Generate PDF from the website
+    HTML(url).write_pdf(pdf_path)
     print("PDF created successfully!")
 except Exception as e:
     print("An error occurred:", str(e))
